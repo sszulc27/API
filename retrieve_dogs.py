678106a660dc43d24b18a3b2e92676ea3d8b3b49
sszulc27 <sszulc27@gmail.com>
2018-06-13 22:44:49 -0400

    Initial commit

---
+++ retrieve_dogs.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://dog.ceo/api/breeds/list/all"
+
+resp = requests.get(url)
+if resp.status_code != 200:
+    # This means something went wrong.
+    print("Error!")
+
+for item in resp.json()['message'].keys():
+    print(item)
